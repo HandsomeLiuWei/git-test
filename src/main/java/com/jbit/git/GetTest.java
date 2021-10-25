b19b768ac568810bd908812ffa285fe165309bad
LiuWei <2245578806@qq.com>
2021-10-26 06:01:18 +0800

    master commit2

--- src/main/java/com/jbit/git/GetTest.java
+++ src/main/java/com/jbit/git/GetTest.java
@@ -13,6 +13,8 @@ public class GetTest {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master commit test");
+
 
     }
 }
