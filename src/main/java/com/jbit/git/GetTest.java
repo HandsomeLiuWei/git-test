526086038247ce24887b6f59647c1517f8284a7b
LiuWei <2245578806@qq.com>
2021-10-26 05:44:12 +0800

    first commit

---
+++ src/main/java/com/jbit/git/GetTest.java
@@ -0,0 +1,12 @@
+package com.jbit.git;
+
+/**
+ * @author LiuWei
+ * @version 1.0
+ * @date 2021/10/26 5:40
+ */
+public class GetTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
