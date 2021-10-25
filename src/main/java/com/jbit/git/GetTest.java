80c5ad0e24edfbc44ba58fc46f188bfc7a313762
LiuWei <2245578806@qq.com>
2021-10-26 05:47:44 +0800

    second commit

--- src/main/java/com/jbit/git/GetTest.java
+++ src/main/java/com/jbit/git/GetTest.java
@@ -8,5 +8,6 @@ package com.jbit.git;
 public class GetTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
